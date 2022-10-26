84357e742c687f97fcc757705a83eac89aa00985
swastikpatro <patroswastik.jsdev@gmail.com>
2022-10-26 20:14:27 +0530

    feat: replaced with social links

--- data.js
+++ data.js
@@ -2,27 +2,63 @@ const sublinks = [
   {
     page: 'products',
     links: [
-      { label: 'payment', icon: 'fas fa-credit-card', url: 'products.html' },
-      { label: 'terminal', icon: 'fas fa-credit-card', url: 'products.html' },
-      { label: 'connect', icon: 'fas fa-credit-card', url: 'products.html' },
+      {
+        label: 'payment',
+        icon: 'fas fa-credit-card',
+        url: 'https://github.com/swastikpatro',
+      },
+      {
+        label: 'terminal',
+        icon: 'fas fa-credit-card',
+        url: 'https://twitter.com/Swastik2001',
+      },
+      {
+        label: 'connect',
+        icon: 'fas fa-credit-card',
+        url: 'https://github.com/swastikpatro',
+      },
     ],
   },
   {
     page: 'developers',
     links: [
-      { label: 'plugins', icon: 'fas fa-book', url: 'products.html' },
-      { label: 'libraries', icon: 'fas fa-book', url: 'products.html' },
-      { label: 'document', icon: 'fas fa-book', url: 'products.html' },
-      { label: 'billing', icon: 'fas fa-book', url: 'products.html' },
-      // { label: 'billing', icon: 'fas fa-book', url: 'products.html' },
-      // { label: 'billing', icon: 'fas fa-book', url: 'products.html' },
+      {
+        label: 'plugins',
+        icon: 'fas fa-book',
+        url: 'https://github.com/swastikpatro',
+      },
+      {
+        label: 'libraries',
+        icon: 'fas fa-book',
+        url: 'https://twitter.com/Swastik2001',
+      },
+      {
+        label: 'document',
+        icon: 'fas fa-book',
+        url: 'https://github.com/swastikpatro',
+      },
+      {
+        label: 'billing',
+        icon: 'fas fa-book',
+        url: 'https://twitter.com/Swastik2001',
+      },
+      // { label: 'billing', icon: 'fas fa-book', url: 'https://github.com/swastikpatro' },
+      // { label: 'billing', icon: 'fas fa-book', url: 'https://github.com/swastikpatro' },
     ],
   },
   {
     page: 'company',
     links: [
-      { label: 'about', icon: 'fas fa-briefcase', url: 'products.html' },
-      { label: 'customers', icon: 'fas fa-briefcase', url: 'products.html' },
+      {
+        label: 'about',
+        icon: 'fas fa-briefcase',
+        url: 'https://github.com/swastikpatro',
+      },
+      {
+        label: 'customers',
+        icon: 'fas fa-briefcase',
+        url: 'https://twitter.com/Swastik2001',
+      },
     ],
   },
 ];
